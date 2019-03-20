128204520eb7661a25868638288155562ed08409
Amr <aah70@zips.uakron.edu>
2019-03-19 21:42:41 -0400

    edit to Proj 3, i believe this is whats needed

--- Project3.py
+++ Project3.py
@@ -6,7 +6,14 @@ class Expr:
         not e1
         e1 and e2
         e1 or e2
+        if e1 then e2 else e3
+        x
+        \x.e1
+        e1 e2
+        e0(e1, e1, ..., en)
 
   /
  
   pass
+
+
